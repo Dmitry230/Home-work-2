9abfd26c36b0fa4c8b53917b248b9d35029a7680
Dmitry230 <quaswex037@gmail.com>
2024-11-07 17:10:48 +0500

    var

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println("Hello world!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
     }
 }
