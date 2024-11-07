569fbcb2f58591871e4c35c07396dd9c89508ce7
Dmitry230 <quaswex037@gmail.com>
2024-11-07 17:30:26 +0500

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,30 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
-        var friend1 = 19;
-        System.out.println(friend1);
-        var friend2 = 19+2;
-        System.out.println(friend2);
-        var friend3 = (19+2)/7;
-        System.out.println(friend3);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend +2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
     }
 }
